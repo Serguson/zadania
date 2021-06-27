993662c21c2c67cd79debe23a18acf9995bf7dd3
Serguson <84004791+Serguson@users.noreply.github.com>
2021-06-27 08:12:58 +0200

    Update app.js

--- 5-dom/1-zadanie-queryselector/js/app.js
+++ 5-dom/1-zadanie-queryselector/js/app.js
@@ -1,2 +1,2 @@
-const firstAttempt=document.querySelectorAll(".first-attempt")
-firstAttempt.classList.add(".active")
+const firstAttempt = document.querySelector(".first-attempt");
+firstAttempt.classList.add(".active");
