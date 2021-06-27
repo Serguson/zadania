80c9fdd05a9c675596e59c652ab4b895041935bb
Serguson <84004791+Serguson@users.noreply.github.com>
2021-06-27 08:34:28 +0200

    Update app.js

--- 5-dom/1-zadanie-queryselector/js/app.js
+++ 5-dom/1-zadanie-queryselector/js/app.js
@@ -2,3 +2,9 @@ const firstAttempt = document.querySelectorAll(".first-attempt");
 for (const el of firstAttempt){
   el.classList.add("active")
 }
+const elements = document.querySelectorAll ("body *")
+elements.forEach(el=>{
+  if (el.tagName==="data-border"){
+  el.setAttribute(data-el-active);
+  }
+})
