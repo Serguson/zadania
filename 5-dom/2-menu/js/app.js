b688e49b82fc6f3499db8b5b3ddf86841970fa18
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-01 12:46:18 +0200

    Update app.js

--- 5-dom/2-menu/js/app.js
+++ 5-dom/2-menu/js/app.js
@@ -1,8 +1,9 @@
 const parent=document.querySelectorAll("li.nav-el");
 const navEl=document.querySelectorAll("a.nav-link");
-let navElActive=document.querySelectorAll("li.nav-el-active");
+
 for (const el of parent){
 el.addEventListener("click", e => {
+                        let navElActive=document.querySelectorAll("li.nav-el-active");
                         e.preventDefault();
                        console.log ("Kliknięto!");
                         navElActive.forEach (active=>active.classList.remove("nav-el-active")); 
