e11bae6d0ccb9ac1ede572dbaa5b7f217841a59a
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-01 11:16:32 +0200

    Update app.js

--- 5-dom/2-menu/js/app.js
+++ 5-dom/2-menu/js/app.js
@@ -1,4 +1,4 @@
 const parent=document.querySelectorAll("li.nav-el");
 const navEl=document.querySelectorAll("a.nav-link");
-navEl.addEventListener("click" e=>{
+navEl.addEventListener("click", e=>{
                        console.log ("Kliknięto!"})
