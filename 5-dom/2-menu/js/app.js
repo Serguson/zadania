6a8039b54209e4901892c10af0179d693999bbd6
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-01 11:42:32 +0200

    Update app.js

--- 5-dom/2-menu/js/app.js
+++ 5-dom/2-menu/js/app.js
@@ -1,7 +1,7 @@
 const parent=document.querySelectorAll("li.nav-el");
 const navEl=document.querySelectorAll("a.nav-link");
 for (const el of parent){
-navEl.addEventListener("click", e => {
+el.addEventListener("click", e => {
                         e.preventDefault();
                        console.log ("Kliknięto!");})
 }
