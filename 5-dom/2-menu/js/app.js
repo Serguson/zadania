e25a22241e17a601e5d31be971671fd4d7184546
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-01 12:42:09 +0200

    Update app.js

--- 5-dom/2-menu/js/app.js
+++ 5-dom/2-menu/js/app.js
@@ -5,6 +5,7 @@ for (const el of parent){
 el.addEventListener("click", e => {
                         e.preventDefault();
                        console.log ("Kliknięto!");
+                        navElActive.forEach (active=>active.classList.remove("nav-el-active"); 
                        e.target.classList.add("nav-el-active");
                      
 })
