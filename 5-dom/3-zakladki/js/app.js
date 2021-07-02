c430bb0c1b390ccdffd994517e7b19b9f70f6208
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-02 10:44:26 +0200

    Update app.js

--- 5-dom/3-zakladki/js/app.js
+++ 5-dom/3-zakladki/js/app.js
@@ -1,5 +1,5 @@
 const tabEl = document.querySelectorAll ("li.tab-el")
 const tabElActive = document.querySelectorAll("li.tab-el-active")
 const tabLink = document.querySelectorAll("a.tab-link")
-const tabContent = document.querySelectorAll("tab-contents.tab-content")
-const tabContentActive = document.querySelectorAll("tab-contents.tab-content-active")
+const tabContent = document.querySelectorAll("div.tab-content")
+const tabContentActive = document.querySelectorAll("div.tab-content-active")
