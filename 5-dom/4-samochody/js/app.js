2340fd563e76f80261184146ab46e7403edfb52a
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-02 12:53:32 +0200

    Update app.js

--- 5-dom/4-samochody/js/app.js
+++ 5-dom/4-samochody/js/app.js
@@ -1,3 +1,3 @@
 const button=document.getElementsByClassName("car-toggle-detail")
 const carDetail=document.getElementsByClassName("car-detail")
-const car=document.getElementsByClassName("car)
+const car=document.getElementsByClassName("car")
