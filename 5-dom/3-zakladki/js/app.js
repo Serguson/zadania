6512b4e2a002adcca81d97da2187c3b4384d01f4
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-02 10:17:14 +0200

    Update app.js

--- 5-dom/3-zakladki/js/app.js
+++ 5-dom/3-zakladki/js/app.js
@@ -1,5 +1,5 @@
-const tabEl = document.querySelector("li.tab-el")
-const tabLink = document.querySelector ("tab-link")
+const tabEl = document.querySelectorAll("li.tab-el")
+const tabLink = document.querySelectorAll("tab-link")
 for (const el of tabLink) {
   el.addEventLister("click", e=> {
     el.classList.add("tab-el-active")})
