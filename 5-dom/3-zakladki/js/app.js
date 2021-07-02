63e6ecc18887c9000aef653e19d3a934f2ffdd1d
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-02 10:47:13 +0200

    Update app.js

--- 5-dom/3-zakladki/js/app.js
+++ 5-dom/3-zakladki/js/app.js
@@ -3,3 +3,5 @@ const tabElActive = document.querySelectorAll("li.tab-el-active")
 const tabLink = document.querySelectorAll("a.tab-link")
 const tabContent = document.querySelectorAll("div.tab-content")
 const tabContentActive = document.querySelectorAll("div.tab-content-active")
+tabLink.addEventListener("click", e=> {
+  console.log("Kliknięto!")})
