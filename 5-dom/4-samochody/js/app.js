d2c0ad1829f29386c6212f0bae33ef1153f327ef
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-02 13:10:36 +0200

    Update app.js

--- 5-dom/4-samochody/js/app.js
+++ 5-dom/4-samochody/js/app.js
@@ -2,3 +2,8 @@ const button=document.getElementsByClassName("car-toggle-detail")
 const carDetail=document.getElementsByClassName("car-detail")
 const car=document.getElementsByClassName("car")
 const carSimpleData=document.getElementsByClassName("car-simple-data")
+for (const el of button){
+  el.addEventListener("click", e => {
+    const detail=button.closest(".car-detail")
+    detail.removeAttribute("style")})
+}
