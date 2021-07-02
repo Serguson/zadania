0f11f4e8db66c2915d20406872451f8412d196a2
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-02 12:07:03 +0200

    Update app.js

--- 5-dom/3-zakladki/js/app.js
+++ 5-dom/3-zakladki/js/app.js
@@ -1,8 +1,8 @@
-const tabEl = document.querySelectorAll ("li.tab-el")
-const tabElActive = document.querySelector("li.tab-el-active")
-const tabLink = document.querySelectorAll("a.tab-link")
-const tabContent = document.querySelectorAll("div.tab-content")
-const tabContentActive = document.querySelectorAll("div.tab-content-active")
+const tabEl = document.getElementsByClassName ("tab-el")
+const tabElActive = document.getElementsByClassName("tab-el-active")
+const tabLink = document.getElementsByClassName("tab-link")
+const tabContent = document.getElementsByClassName("tab-content")
+const tabContentActive = document.getElementsByClassName("tab-content-active")
 for (const el of tabEl) {el.addEventListener("click", e=> {
   console.log("Kliknięto!");
   tabElActive.classList.remove("tab-el-active");
