35c5bb667b4312787f5b4846a55792de3e8909dd
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-02 13:21:33 +0200

    Update app.js

--- 5-dom/4-samochody/js/app.js
+++ 5-dom/4-samochody/js/app.js
@@ -4,5 +4,5 @@ const car=document.getElementsByClassName("car")
 const carSimpleData=document.getElementsByClassName("car-simple-data")
 for (const el of button){
   el.addEventListener("click", e => {
-    const detail=el.closest(".car")
-})
+    const detail=el.closest(".car")}
+)}
