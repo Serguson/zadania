12d12e8e4ab681e7378ac893d412fabb70ab4a7a
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-02 11:55:19 +0200

    Update app.js

--- 5-dom/2-menu/js/app.js
+++ 5-dom/2-menu/js/app.js
@@ -1,4 +1,11 @@
 const navEl=document.getElementsByClassName("nav-el");
 const navLink=document.getElementsByClassName("nav-link");
 let navElActive=document.getElementsByClassName("nav-el-active");
-
+for (const el of navLink){
+  el.addEventListener("click", e => {
+    e.preventDefault();
+    navElActive.classList.remove("nav-el-active");
+    el.parentElement.classList.toggle("nav-el-active");
+  })
+}
+    
