a87811af78099cad80476642e236740743269f51
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-02 14:35:52 +0200

    Update app.js

--- 5-dom/4-samochody/js/app.js
+++ 5-dom/4-samochody/js/app.js
@@ -3,11 +3,11 @@ const carDetail=document.getElementsByClassName("car-detail")
 const car=document.getElementsByClassName("car")
 const carSimpleData=document.getElementsByClassName("car-simple-data")
 
-for (const el of car){
+for (const el of button){
   el.addEventListener("click", e => {
     console.log(e);
     console.log("car");
-    if (e.originalTarget.getAttribute("type")==="button"){
-      console.log("jest!")}
-  }, true
+    const car = el.closest(".car");
+    car.children[1].removeAttribute("style");
+  }
 )}
