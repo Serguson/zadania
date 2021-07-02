4c2ccfec48cdb658fb1ef8f4fcb2d6970b9dd9a9
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-02 10:28:51 +0200

    Update app.js

--- 5-dom/3-zakladki/js/app.js
+++ 5-dom/3-zakladki/js/app.js
@@ -1,5 +1,5 @@
 const tabEl = document.querySelectorAll("li.tab-el")
-const tabLink = document.querySelectorAll("tab-link")
+const tabLink = document.querySelectorAll("li.a.tab-link")
 for (const el of tabEl) {
   el.addEventLister("click", e=> {
    console.log("Kliknięto")})
