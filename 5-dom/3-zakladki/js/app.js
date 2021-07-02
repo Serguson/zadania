81ae07f8611fb590318051661148536578bdb789
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-02 12:42:18 +0200

    Update app.js

--- 5-dom/3-zakladki/js/app.js
+++ 5-dom/3-zakladki/js/app.js
@@ -5,9 +5,13 @@ const tabContent = document.getElementsByClassName("tab-content")
 const tabContentActive = document.getElementsByClassName("tab-content-active")
 const js = document.getElementById("js")
 const python = document.getElementById("python")
-const TurboPascal = document.getElementById("turbo-pascal")
+const turboPascal = document.getElementById("turbo-pascal")
 for (const el of tabEl) {el.addEventListener("click", e=> {
   console.log("Kliknięto!");
   for (const el of tabElActive) {el.classList.remove("tab-el-active")};
   el.classList.add("tab-el-active");
+  for (const el of tabContentActive){el.classList.remove("tab-content-active")};
+  if (el.firstElementChild.getAttribute("href")==="#js"){
+    js.classList.add("tab-content-active")} else if (el.firstElementChild.getAttribute("href")==="#python"){
+      python.classList.add("tab-content-active")} else {turboPascal.classList.add("tab-content-active")};
 })}
