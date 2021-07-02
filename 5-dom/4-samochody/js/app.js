fdec2e71a405009347d28914d5dbf6e0440f42e6
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-02 14:13:53 +0200

    Update app.js

--- 5-dom/4-samochody/js/app.js
+++ 5-dom/4-samochody/js/app.js
@@ -2,13 +2,7 @@ const button=document.getElementsByClassName("car-toggle-detail")
 const carDetail=document.getElementsByClassName("car-detail")
 const car=document.getElementsByClassName("car")
 const carSimpleData=document.getElementsByClassName("car-simple-data")
-for (const el of button){
-  el.addEventListener("click", e => {
-    console.log(e);
-    console.log("button");
-    }
-               
-)}
+
 for (const el of car){
   el.addEventListener("click", e => {
     console.log(e);
