c59b0b8c7b28f8225f5ba0f2452940a63231d575
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-02 12:17:23 +0200

    Update app.js

--- 5-dom/3-zakladki/js/app.js
+++ 5-dom/3-zakladki/js/app.js
@@ -3,6 +3,9 @@ const tabElActive = document.getElementsByClassName("tab-el-active")
 const tabLink = document.getElementsByClassName("tab-link")
 const tabContent = document.getElementsByClassName("tab-content")
 const tabContentActive = document.getElementsByClassName("tab-content-active")
+const js = document.getElementByiD("js")
+const python = document.getElementById("python")
+const TurboPascal = document.getElementById("turbo-pascal")
 for (const el of tabEl) {el.addEventListener("click", e=> {
   console.log("Kliknięto!");
   for (const el of tabElActive) {el.classList.remove("tab-el-active")};
