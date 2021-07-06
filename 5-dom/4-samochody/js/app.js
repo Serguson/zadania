a9ca31d40b53b94f18de99750ba18a0e4aadd64b
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-06 09:45:09 +0200

    Update app.js

--- 5-dom/4-samochody/js/app.js
+++ 5-dom/4-samochody/js/app.js
@@ -5,7 +5,7 @@ const carSimpleData=document.getElementsByClassName("car-simple-data")
 for (const el of button) {
   el.addEventListener("click", e => {
     if (el.innerText==="POKAŻ DETALE") {el.innerText="SCHOWAJ DETALE"} else {el.innerText="POKAŻ DETALE"};
-    if (el.closest(".car").children[1].style.display==="none") {el.closest(".car).children[1].style.display="block"} else {el.closest(".car).children[1].style.display="none"};
+    if (el.closest(".car").children[1].style.display==="none") {el.closest(".car").children[1].style.display="block"} else {el.closest(".car").children[1].style.display="none"};
   })}
                                                                             
 
