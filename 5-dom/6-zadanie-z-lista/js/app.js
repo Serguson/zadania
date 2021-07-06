2a21af39448fc10b000a2ccec6f5630a3ed581d2
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-06 13:26:21 +0200

    Update app.js

--- 5-dom/6-zadanie-z-lista/js/app.js
+++ 5-dom/6-zadanie-z-lista/js/app.js
@@ -1,2 +1,11 @@
 const add=document.querySelector("#add");
 const list=document.querySelector(".list");
+
+function addElement (nr) {
+  const newElement=document.createElement("div");
+  newElement.classList.add("element");
+  const newElementInner = document.querySelector("#element-list").content.cloneNode(true);
+  newElement.append(newElementInner);
+  newElement.querySelector(".element-title").innerText="Element nr ";
+  list.append(newElement);
+}
