6adf58271efe8d892afa371cbc985df101b6eeac
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-06 11:07:37 +0200

    Update app.js

--- 5-dom/5-lista-userow/js/app.js
+++ 5-dom/5-lista-userow/js/app.js
@@ -1,9 +1,23 @@
+document.addEventListener("DOMContentLoaded", e => {
 const user=document.getElementsByClassName("user")
 const form=document.getElementsByClassName("form")
 const userList=document.getElementsByClassName("user-list")
-for (const el of form) {
-  el.addEventListener("submit", e => {
-    const newUser=document.createElement("li");
-    newUser.classList.add("user");
-    userList.appendChild(newUser);
-  })}
+const name=document.querySelector("#name");
+const phone=document.querySelector("#phone");
+})
+function addUser (name, phone) {
+  const newUser=document.createElement("li");
+  newUser.classList.add("user");
+  const newUserInner=document.querySelector("#newUser").content.cloneNode(true);
+  element.append(newUserInner);
+  element.querySelector(".user-name").innerText=name;
+  element.querySelector(".user-phone").innerText=phone;
+  userList.append(newUser);
+}
+form.addEventListener("submit", e => {
+    e.preventDefault();
+  if (name.value!=="" && phone.value!=="") {
+    addUser (name.value, phone.value);
+    name.value=""
+    phone.value=""
+  }})
