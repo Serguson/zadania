27c016e3dc3fcbab169c9d4a843f91a18fabf79e
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-06 08:43:17 +0200

    Update app.js

--- 5-dom/4-samochody/js/app.js
+++ 5-dom/4-samochody/js/app.js
@@ -3,13 +3,3 @@ const carDetail=document.getElementsByClassName("car-detail")
 const car=document.getElementsByClassName("car")
 const carSimpleData=document.getElementsByClassName("car-simple-data")
 
-for (const el of button){
-  el.addEventListener("click", e => {
-    console.log(e);
-    console.log("car");
-    const car = el.closest(".car");
-     if(car.children[1].hasAttribute("style")){car.children[1].removeAttribute("style")} else{car.children[1].addAttribute("style")};
-    if (this.textContent==="\n                            Pokaż detale\n                        ") {this.textContent="\n                            Schowaj detale\n                        "} else {this.textContent="\n                            Pokaż detale\n                        "};
-    car.classList.toggle("car-show-detail");
-  }
-)}
