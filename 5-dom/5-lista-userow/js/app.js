b0108fe9b5378d26f43bb3b7068dc8316b77155a
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-06 11:49:51 +0200

    Update app.js

--- 5-dom/5-lista-userow/js/app.js
+++ 5-dom/5-lista-userow/js/app.js
@@ -1,5 +1,5 @@
 const user=document.getElementsByClassName("user");
-const form=document.querySelector("#form");
+const form=document.querySelector("form");
 const userList=document.getElementsByClassName("user-list");
 const name=document.querySelector("#name");
 const phone=document.querySelector("#phone");
