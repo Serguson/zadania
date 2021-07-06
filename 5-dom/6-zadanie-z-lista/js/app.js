2b85903314e32b32e86742bac80ed1e5c90550da
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-06 13:51:38 +0200

    Update app.js

--- 5-dom/6-zadanie-z-lista/js/app.js
+++ 5-dom/6-zadanie-z-lista/js/app.js
@@ -7,7 +7,7 @@ function addElement (nr) {
   newElement.classList.add("element");
   const newElementInner = document.querySelector("#element-list").content.cloneNode(true);
   newElement.append(newElementInner);
-  newElement.querySelector(".element-title").innerHtml=`Element numer <span class="nr">${counter}</span>`;
+  newElement.querySelector(".element-title").innerHtml="Element numer <span class="nr">${counter}</span>";
   list.append(newElement);
 }
 
