5749a95088989c5053888909ee95c8c849790d79
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-06 14:57:23 +0200

    Update app.js

--- 5-dom/6-zadanie-z-lista/js/app.js
+++ 5-dom/6-zadanie-z-lista/js/app.js
@@ -17,16 +17,9 @@ add.addEventListener("click", e => {
   addElement(counter++);
 })
 
-for (const el of listSize) {
-  el.addEventListener("click", e => {
-    if (e.target.classList.contains("clone")){
-       const newElement=document.createElement("div");
-       newElement.classList.add("element");
-       const newElementInner = document.querySelector("#element-list").content.cloneNode(true);
-       newElement.append(newElementInner);
-       list.append(newElement);
-    } else if (e.target.classList.contains("delete")) {
-               e.target.closest(".element").remove();}
-  })}
-       
+for (const el of listSize){
+  el.addEventListener ("click", e => {
+   if (e.target.classList.contains("delete")){
+     e.target.closest(".element").remove();}
+  })
         
