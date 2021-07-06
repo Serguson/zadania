60a2efbf25a6b74ce96e4ce3fcd2ba5473d93705
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-06 10:00:24 +0200

    Update app.js

--- 5-dom/5-lista-userow/js/app.js
+++ 5-dom/5-lista-userow/js/app.js
@@ -2,7 +2,7 @@ const user=document.getElementByClassName("user")
 const form=document.getElementByClassName("form")
 const userList=document.getElementByClassName("user-list")
 for (const el of form) {
-el.addEventListener("submit" {
+el.addEventListener("submit", {
   document.createElement("li");
   userList.appendChild("li")
   })
