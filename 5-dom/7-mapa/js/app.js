48ad23c0fe07294f98b7c55d74b33ef4226f2969
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-07 14:01:36 +0200

    Update app.js

--- 5-dom/7-mapa/js/app.js
+++ 5-dom/7-mapa/js/app.js
@@ -1,3 +1,10 @@
-for (i=0; i>cities.length; i++){
+const map=document.querySelector(".map");
+cities.forEach(el => {
 const element=document.createElement("a");
-element.setAttribute("href", "cities[i].href")}
+element.setAttribute("href", "el.href");
+element.classList.add("map-marker");
+element.dataset.name="el.name";
+element.dataset.population="el.population";
+element.style.cssText=left: 180px; top: 270px;
+map.appendChild(element);
+})
