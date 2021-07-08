a15a9930e494e7fc60261498dcfb1395a9f8c289
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-08 14:13:48 +0200

    Update app.js

--- 5-dom/7-mapa/js/app.js
+++ 5-dom/7-mapa/js/app.js
@@ -1,4 +1,9 @@
 const map=document.querySelector(".map");
+const mapTooltip=document.createElement("div");
+map.appendChild(mapTooltip);
+mapTooltip.classList.add("map-tooltip");
+mapTooltip.style="left: -9999px; top: -9999px";
+const mapMarkers = document.getElementsByClassName("map-marker")
 
 cities.forEach (el => {
   const {
@@ -7,39 +12,19 @@ cities.forEach (el => {
     map_x,
     map_y,
     href,} = el;
-const element=document.createElement("a");
-element.setAttribute("href", `${href}`);
-element.classList.add("map-marker");
-element.dataset.name=`${name}`;
-element.dataset.population=`${population}`;
-console.log(`${map_x}`);
-element.style=`left: ${map_x}px; top: ${map_y}px`;
-map.appendChild(element);
-})
-
-
-
-cities.forEach (
-  el => {
-   const {
-    name,
-    population,
-    map_x,
-    map_y,
-    href,} = el;
-    const mapTooltip=document.createElement("div");
-    map.appendChild(mapTooltip);
-    mapTooltip.classList.add("map-tooltip");
-    mapTooltip.style="left: -9999px; top: -9999px";
-    const mapMarkers = document.getElementsByClassName("map-marker");
-    for (const element of mapMarkers) 
-    {
-       element.addEventListener("mouseover", e =>
-                                {
+  const element=document.createElement("a");
+  element.setAttribute("href", `${href}`);
+  element.classList.add("map-marker");
+  element.dataset.name=`${name}`;
+  element.dataset.population=`${population}`;
+  console.log(`${map_x}`);
+  element.style=`left: ${map_x}px; top: ${map_y}px`;
+  map.appendChild(element);
+  for (const element of mapMarkers) {
+       element.addEventListener("mouseover", e =>{
           console.log(e);
           mapTooltip.innerHTML=`<h2>${name}</h2> <div>Population: <strong>${population}</strong></div>`;
           mapTooltip.style.left=`${e.pageX}+30px`;
           mapToltip.style.top=`${e.pageY}+30px`;
-       }
-                           )
-    } })
+       })}
+})
