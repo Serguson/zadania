a0b65041e9751127180b1878edfae56783f0627a
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-08 14:35:40 +0200

    Update app.js

--- 5-dom/7-mapa/js/app.js
+++ 5-dom/7-mapa/js/app.js
@@ -23,12 +23,13 @@ cities.forEach (el => {
 })
 
 for (const element of mapMarkers) {
-    const {
-    name,
-    population,
-    map_x,
-    map_y,
-    href,} = el;
+    cities.forEach (el => {
+      const {
+       name,
+       population,
+       map_x,
+       map_y,
+       href,} = el;})
        element.addEventListener("mouseover", e =>{
           console.log(e);
           mapTooltip.innerHTML=`<h2>${name}</h2> <div>Population: <strong>${population}</strong></div>`;
