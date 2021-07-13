7bb226b1ab6c1abc91b87710c04eb086fe7a083a
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-13 10:37:31 +0200

    Update app.js

--- 5-dom/8-menu-scroll/js/app.js
+++ 5-dom/8-menu-scroll/js/app.js
@@ -1,9 +1,9 @@
 const paragraph=document.getElementsByClassName("nav-el")
-const activeParagraph=document.querySelector(".nav-el-active")
+const activeParagraph=document.getElementsByClassName("nav-el-active")
 const navLink=document.getElementsByClassName("nav-link")
 for (const el of navLink){
   el.addEventListener("click", e=> {
-    activeParagraph.classList.remove("nav-el-active");
+    for (const el of activeParagraph) {el.classList.remove("nav-el-active")};
     console.log(e);
     el.parentElement.classList.toggle("nav-el-active");
     })
