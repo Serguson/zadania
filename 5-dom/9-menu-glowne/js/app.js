cba4e0ad286966ea1ea15c4ee14071e1149071a2
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-13 16:47:45 +0200

    Update app.js

--- 5-dom/9-menu-glowne/js/app.js
+++ 5-dom/9-menu-glowne/js/app.js
@@ -1,4 +1,12 @@
 const menu=document.getElementById("menu")
 menu.classList.add("menu");
-const a=menu.querySelectorAll("a");
-const li=menu.querySelectorAll("li");
+const a=menu.getElementsByTagName("a");
+const li=menu.getElementsByTagName("li");
+for (const el of li){
+  el.addEventListener("mouseover", e => {
+    console.log(e);
+    for (const element of li) {element.classList.remove("active")};
+    el.classList.toggle("active")
+  })
+}
+    
