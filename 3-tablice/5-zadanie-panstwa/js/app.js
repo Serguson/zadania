692c456bb42ca94284beff306486a53c0a79abfc
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-29 14:20:36 +0200

    Update app.js

--- 3-tablice/5-zadanie-panstwa/js/app.js
+++ 3-tablice/5-zadanie-panstwa/js/app.js
@@ -10,9 +10,12 @@ countries.forEach(el =>
   console.log (`Średnia liczba ludności wynosi ${averagePopulation.toFixed(2)}`)
 
     let growPositive=[]
+    let growNegative=[]
 countries.forEach(el => 
   { 
     const {id, name, population, grow, net_change, density, land_area_in_km, fertility_rate, medium_age, urban_population_in_percent, world_area_in_percent} = el;
-    if (grow>=0) {growPositive.push(el)}
+    if (grow>=0) {growPositive.push(el)} else {growNegative.push(el)};
   })
 console.log (`Jest ${growPositive.length} państw z dodatnim przyrostem naturalnym.`)
+console.log (`Jest ${growNegative.length} państw z ujemnym przyrostem naturalnym.`)
+
