a7b586a0a940db792eb89dedf2931484f51be13c
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-29 14:35:27 +0200

    Update app.js

--- 3-tablice/5-zadanie-panstwa/js/app.js
+++ 3-tablice/5-zadanie-panstwa/js/app.js
@@ -1,17 +1,20 @@
     let sumPopulation=0
     let sumPercent=0
     let sumFertility=0
+    let sumAge=0
 countries.forEach(el => 
   { 
     const {id, name, population, grow, net_change, density, land_area_in_km, fertility_rate=null, medium_age, urban_population_in_percent, world_area_in_percent} = el;
     sumPopulation+=population;
     sumPercent+=world_area_in_percent;
     sumFertility+=fertility_rate;
+    sumAge+=age;
     console.log (name);
   })
   console.log (`Liczba ludności wszystkich państw wynosi ${sumPopulation}.`)
   const averagePopulation = sumPopulation/countries.length
   const averageFertility=sumFertility/countries.length
+  const averageAge=sumAge/countries.length
   console.log (`Średnia liczba ludności wynosi ${averagePopulation.toFixed(2)}`)
 
     let growPositive=[]
@@ -24,5 +27,6 @@ countries.forEach(el =>
 console.log (`Jest ${growPositive.length} państw z dodatnim przyrostem naturalnym.`)
 console.log (`Jest ${growNegative.length} państw z ujemnym przyrostem naturalnym.`)
 console.log(`Suma zajmowanej powierzchni ziemi przez państwa wynosi ${sumPercent.toFixed(2)} procenta.`)
-console.log(`Średnia dzietność na świecie wynosi ${averageFertility.toFixed(2)}`)
+console.log(`Średnia dzietność na świecie wynosi ${averageFertility.toFixed(2)}.`)
+console.log (`Średnia wieku na świecie wynosi ${averageAge.toFixed(2)}.`)
 
