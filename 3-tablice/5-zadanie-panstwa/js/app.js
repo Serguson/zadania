983c5ef5e4f423f70a3955e5a0848c507df22289
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-29 14:06:13 +0200

    Update app.js

--- 3-tablice/5-zadanie-panstwa/js/app.js
+++ 3-tablice/5-zadanie-panstwa/js/app.js
@@ -1,8 +1,8 @@
+    let sum=0
 countries.forEach(el => 
   { 
-    let sum=0
     const {id, name, population, grow, net_change, density, land_area_in_km, fertility_rate, medium_age, urban_population_in_percent, world_area_in_percent} = el;
     sum+=population;
     console.log (name);
   })
-  console.log (sum)
+  console.log (`Liczba ludności wszystkich państw wynosi ${sum}.)
